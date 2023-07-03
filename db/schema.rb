360a2428e614ae088e54aa9e74683ffc8f6507d9
EC2 Default User <itakaha71@gmail.com>
2023-07-03 13:54:19 +0000

    [Fix]db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_06_24_133752) do
+ActiveRecord::Schema.define(version: 2023_07_03_135334) do
 
   create_table "players", force: :cascade do |t|
     t.string "name"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2023_06_24_133752) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "address"
+    t.integer "player_id"
   end
 
 end
